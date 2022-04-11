9b66fe6f65c85be99031760813a417e20908379a
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-04-11 06:38:15 +0600

    expert added with responsive design

--- Milestone 10/genius-car-services/src/Pages/Home/Home/Home.js
+++ Milestone 10/genius-car-services/src/Pages/Home/Home/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import Experts from '../Experts/Experts';
 import Services from '../Services/Services';
 
 const Home = () => {
      return (
           <div>
-               <Services>
-
-               </Services>
+               <Services />
+               <Experts />
           </div>
      );
 };
